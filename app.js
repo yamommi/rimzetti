0970004e447640ca522b6c24cb4ee7a4e4967dbd
rimzetti rims <54206026+yamommi@users.noreply.github.com>
2026-03-02 12:40:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,104 @@ const PRODUCTS = [
     sizes: ["18 inch", "19 inch", "20 inch"],
     colors: ["Hyper Black"],
     lugs: ["5-lug"]
-  }
+  },
+ const PRODUCTS = [
+  {
+    itemNo: "001",
+    name: "Forged Rims",
+    img: "images/image-1.png",
+    sizes: ["18 inch", "19 inch", "20 inch", "22 inch"],
+    colors: ["Silver"],
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "002",
+    name: "Rimzetti Apex Cast",
+    img: "images/image-2.png",
+    sizes: ["18 inch", "19 inch", "20 inch"],
+    colors: ["Black"],
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "003",
+    name: "Rimzetti Factory Edition",
+    img: "images/image-3.png",
+    sizes: ["18 inch", "19 inch", "20 inch"],
+    colors: ["Hyper Black"],
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "004",
+    name: "Rimzetti Series 4",
+    img: "images/image-4-black.png", // This is the main display image
+    sizes: ["18 inch", "19 inch", "20 inch"],
+    colors: ["Black", "Bronze", "Red"], // These will show up in the pop-up
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "005",
+    name: "Rimzetti Sport Series",
+    img: "images/image-5.png",
+    sizes: ["18 inch", "19 inch"],
+    colors: ["Silver", "Gunmetal"],
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "006",
+    name: "Rimzetti深 Rim Deep",
+    img: "images/image-6.png",
+    sizes: ["20 inch", "22 inch"],
+    colors: ["Chrome", "Black"],
+    lugs: ["5-lug", "6-lug"]
+  },
+  {
+    itemNo: "007",
+    name: "Rimzetti Velocity",
+    img: "images/image-7.png",
+    sizes: ["19 inch", "20 inch"],
+    colors: ["Gloss Black"],
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "008",
+    name: "Rimzetti Circuit",
+    img: "images/image-8.png",
+    sizes: ["18 inch", "19 inch"],
+    colors: ["Bronze", "Silver"],
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "009",
+    name: "Rimzetti Heritage",
+    img: "images/image-9.png",
+    sizes: ["17 inch", "18 inch", "19 inch"],
+    colors: ["Gold", "Silver"],
+    lugs: ["4-lug", "5-lug"]
+  },
+  {
+    itemNo: "010",
+    name: "Rimzetti Executive",
+    img: "images/image-10.png",
+    sizes: ["20 inch", "21 inch"],
+    colors: ["Brushed Steel"],
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "011",
+    name: "Rimzetti Monoblock",
+    img: "images/image-11.png",
+    sizes: ["19 inch", "20 inch"],
+    colors: ["Matte Black"],
+    lugs: ["5-lug"]
+  },
+  {
+    itemNo: "012",
+    name: "Rimzetti Track Edition",
+    img: "images/image-12.png",
+    sizes: ["18 inch", "19 inch"],
+    colors: ["Hyper Silver"],
+    lugs: ["5-lug"]
+  }}
 ];
 
 
